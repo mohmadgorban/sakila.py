120c335d32a7dbe637e0bd3d2e5330578933b007
mohmad gorban <mohmad.gorban.mo@gmail.com>
2024-07-22 19:30:23 +0300

    import table

--- sakila.py
+++ sakila.py
@@ -5,3 +5,18 @@ from sqlalchemy import create_engine
 
 # 2. Connect to Sakila DB
 engine = db.create_engine('postgresql+psycopg2://app_student:$vaoNXn3^Rmm@hu-dm.postgres.database.azure.com:5432/sakila')
+
+# 3. import table 
+with engine.connect() as connection:
+    actor_df = pd.read_sql_table('actor', connection)
+    film_df = pd.read_sql_table('film', connection)
+    film_actor_df = pd.read_sql_table('film_actor', connection)
+    category_df = pd.read_sql_table('category', connection)
+    film_category_df = pd.read_sql('film_category',connection)
+    payment_df = pd.read_sql_table('payment', connection)
+    customer_df = pd.read_sql_table('customer', connection)
+    store_df = pd.read_sql_table('store', connection)
+    inventory_df = pd.read_sql_table('inventory', connection)
+    rental_df = pd.read_sql_table('rental', connection)
+
+print(rental_df.head(3))
