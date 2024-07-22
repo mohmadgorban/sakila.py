22696889b1b186cfaf87dd806ff007b6dd55444f
mohmad gorban <mohmad.gorban.mo@gmail.com>
2024-07-22 20:54:13 +0300

    Which categories have most films

--- sakila.py
+++ sakila.py
@@ -2,6 +2,8 @@ import pandas as pd
 import seaborn as sns 
 import sqlalchemy as db
 from sqlalchemy import create_engine
+import matplotlib.pyplot as plt
+
 
 # 2. Connect to Sakila DB
 engine = db.create_engine('postgresql+psycopg2://app_student:$vaoNXn3^Rmm@hu-dm.postgres.database.azure.com:5432/sakila')
@@ -23,3 +25,24 @@ with engine.connect() as connection:
 #  1. Are there any duplicates in actor data? If so, drop them from the data.
 
 actor_df = actor_df.drop_duplicates()
+
+# 2. Which categories have most films?
+
+films = film_df.merge(film_category_df, how='left', on='film_id')
+films = films.merge(category_df, how='left', on='category_id')
+
+films_group_by_category = films.groupby(['category_id','name']).agg(count_rows = ('film_id' , 'count')).reset_index()
+
+
+films_group_by_category.set_index('name', inplace=True)
+
+plt.figure(figsize=(10, 6))
+sns.barplot(x='name', y='count_rows', data=films_group_by_category, palette='viridis')
+
+plt.xlabel('Category')
+plt.ylabel('Count')
+plt.title('Count of Rows by Category')
+
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
